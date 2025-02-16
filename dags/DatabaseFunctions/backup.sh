3c679295ee5d618dfe79ff05dc5d0184fcd2f811
Ashley <ashleytyagi@hotmail.com>
2025-02-16 11:47:04 +0000

    update date message

--- dags/DatabaseFunctions/backup.sh
+++ dags/DatabaseFunctions/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-$TODAY=`date --iso-8601` 
+date=$(date '+%Y-%m-%d')
 
-pg_dump ashley_train_prj_db > "prj-database-${TODAY}"
+pg_dump ashley_train_prj_db > "prj-database-${date}"
 
 if [ "$?"-ne 0]; then echo "Help" | mail -s "Backup failed" you@example.com; exit 1; fi
