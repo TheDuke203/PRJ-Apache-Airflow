7ea671475428ff2581ea48df0f707ce7d68a7ac3
Ashley <ashleytyagi@hotmail.com>
2025-02-16 07:34:48 +0000

    use pickle

--- dags/Training/ModelTraining.py
+++ dags/Training/ModelTraining.py
@@ -10,6 +10,7 @@ from sklearn.metrics import accuracy_score
 from DatabaseFunctions.ModelResults import push_model_results
 from datetime import datetime
 from pathlib import Path
+import pickle
 '''
 Example of data coming from server
 (44443, 0, False, 97372160, 25562584, datetime.time(21, 5), datetime.date(2025, 2, 8), 7853, 44443,
@@ -51,11 +52,11 @@ def train_model_from_database():
     print("Building on: " + rows + " rows of data")
     
     clf_model, accuracy, true_ratio = test_train_cancelled_classification(X,y_cancelled)
-    clf_model.booster_.save_model(Path(f'models/lgb_classifier_{datetime.now().strftime("%Y-%m-%d")}.txt'))
+    pickle.dump(clf_model, open(Path(f'models/lgb_classifier_{datetime.now().strftime("%Y-%m-%d")}.txt'), "wb"))
     
     reg_model, r_squared = test_train_delay_regression(X, y_delay)
     
-    reg_model.booster_.save_model(Path(f'models/lgb_regressor_{datetime.now().strftime("%Y-%m-%d")}.txt'))
+    pickle.dump(reg_model, open(Path(f'models/lgb_regressor_{datetime.now().strftime("%Y-%m-%d")}.txt')), 'wb')
 
     print("Regresssion: R_squared result is: "+ str(r_squared))
     print("Classification: Accuracy is: " + str(accuracy))
